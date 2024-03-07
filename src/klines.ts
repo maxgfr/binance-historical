efffe65073289fbd13483b6ae472e3c75c6468c6
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-03-07 15:38:13 +0100

    fix: en cours

--- src/klines.ts
+++ src/klines.ts
@@ -16,46 +16,33 @@ export async function getKline(
   version = 'v3',
   limit = 1000,
 ): Promise<Array<Kline>> {
-  if (startDate.getTime() > endDate.getTime()) {
-    throw new Error('Start date must be before end date');
-  }
-  const numberOfCall = calculateNumberOfCall(
-    interval,
-    startDate.getTime(),
-    endDate.getTime(),
-  );
-  const numOfIterations = Math.ceil(numberOfCall / limit);
-  const divisions = divideInterval(
-    numOfIterations,
-    startDate.getTime(),
-    endDate.getTime(),
-  );
-
-  let data: Array<BinanceResponseData> = [];
-  for (let i = 0; i < divisions.length; i++) {
-    if (i + 1 !== divisions.length) {
-      const start = divisions[i];
-      const end = divisions[i + 1];
-      const url = `https://${source}.binance.com/${source}/${version}/klines?symbol=${pair}&interval=${interval}&startTime=${start}&endTime=${end}&limit=${limit}`;
-      console.log(url);
-      const response: BinanceResponse = await axios.get(url);
-      data = [...data, ...response.data];
+  try {
+    if (startDate.getTime() > endDate.getTime()) {
+      throw new Error('Start date must be before end date');
     }
-  }
+    const numberOfCall = calculateNumberOfCall(
+      interval,
+      startDate.getTime(),
+      endDate.getTime(),
+    );
+    const numOfIterations = Math.ceil(numberOfCall / limit);
+    const divisions = divideInterval(
+      numOfIterations,
+      startDate.getTime(),
+      endDate.getTime(),
+    );
 
-  if (data.length) --data.length;
-  return data.map((item) => ({
-    openTime: item[0],
-    open: item[1],
-    high: item[2],
-    low: item[3],
-    close: item[4],
-    volume: item[5],
-    closeTime: item[6],
-    quoteAssetVolume: item[7],
-    trades: item[8],
-    takerBaseAssetVolume: item[9],
-    takerQuoteAssetVolume: item[10],
-    ignored: item[11],
-  }));
+    let data: Array<BinanceResponseData> = [];
+    for (let i = 0; i < divisions.length; i++) {
+      if (i + 1 !== divisions.length) {
+        const start = divisions[i];
+        const end = divisions[i + 1];
+        const url = `https://${source}.binance.com/${source}/${version}/klines?symbol=${pair}&interval=${interval}&startTime=${start}&endTime=${end}&limit=${limit}`;
+        const response: BinanceResponse = await axios.get(url);
+        data = [...data, ...response.data];
+      }
+    }
+  } catch (error: any) {
+    throw new Error(JSON.stringify(error));
+  }
 }
