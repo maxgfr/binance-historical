17e705e04aaa60a9cce7762c1c348bd043da4a42
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-03-07 15:52:58 +0100

    fix: en cours

--- src/__tests__/klines.test.ts
+++ src/__tests__/klines.test.ts
@@ -1,7 +1,27 @@
 import { getKline } from '../klines';
+import axios from 'axios';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('klines', () => {
   test('Fetch max length', async () => {
+    mockedAxios.get.mockResolvedValue({
+      data: Array(2200).fill([
+        1578524400000,
+        '100.0',
+        '200.0',
+        '50.0',
+        '150.0',
+        '1000.0',
+        1589151600000,
+        '100000.0',
+        100,
+        '1000.0',
+        '100000.0',
+        '1000.0',
+      ]),
+    });
     const result = await getKline(
       'ETHUSDT',
       '4h',
@@ -12,6 +32,22 @@ describe('klines', () => {
   });
 
   test('Fetch for one year', async () => {
+    mockedAxios.get.mockResolvedValue({
+      data: Array(365).fill([
+        1578524400000,
+        '100.0',
+        '200.0',
+        '50.0',
+        '150.0',
+        '1000.0',
+        1589151600000,
+        '100000.0',
+        100,
+        '1000.0',
+        '100000.0',
+        '1000.0',
+      ]),
+    });
     const result = await getKline(
       'ETHUSDT',
       '1d',
