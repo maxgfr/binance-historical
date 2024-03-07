aed56f3288ffce1481540d305966cae717481552
Maxime Golfier <25312957+maxgfr@users.noreply.github.com>
2024-03-07 15:53:41 +0100

    fix(endpoint): set version 3 (#252)
    * fix: en cours
    
    * fix: en cours
    
    * fix: en cours
    
    * fix: en cours
    
    * fix: en cours

--- src/__tests__/klines.test.ts
+++ src/__tests__/klines.test.ts
@@ -1,18 +1,53 @@
 import { getKline } from '../klines';
+import axios from 'axios';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('klines', () => {
   test('Fetch max length', async () => {
+    mockedAxios.get.mockResolvedValue({
+      data: Array(2200).fill([
+        1578524400000,
+        '100.0',
+        '200.0',
+        '50.0',
+        '150.0',
+        '1000.0',
+        1589151600000,
+        '100000.0',
+        100,
+        '1000.0',
+        '100000.0',
+        '1000.0',
+      ]),
+    });
     const result = await getKline(
       'ETHUSDT',
       '4h',
       new Date('01-09-2020'),
       new Date('01-12-2021'),
     );
-    console.log(result);
     expect(result.length).toBeGreaterThan(2200);
   });
 
   test('Fetch for one year', async () => {
+    mockedAxios.get.mockResolvedValue({
+      data: Array(365).fill([
+        1578524400000,
+        '100.0',
+        '200.0',
+        '50.0',
+        '150.0',
+        '1000.0',
+        1589151600000,
+        '100000.0',
+        100,
+        '1000.0',
+        '100000.0',
+        '1000.0',
+      ]),
+    });
     const result = await getKline(
       'ETHUSDT',
       '1d',
